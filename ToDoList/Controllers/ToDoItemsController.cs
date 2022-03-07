cd0c4de5f5d34af13e5d283247f6df4e56526a47
peterpociask <github@pociask.com>
2022-03-07 17:04:03 -0500

    Initial commit

---
+++ ToDoList/Controllers/ToDoItemsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Data;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoItemsController : ControllerBase
+    {
+        private readonly ToDoListContext _context;
+
+        public ToDoItemsController(ToDoListContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ToDoItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDoItem>>> GetToDoItem()
+        {
+            return await _context.ToDoItem.ToListAsync();
+        }
+
+        // GET: api/ToDoItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDoItem>> GetToDoItem(long id)
+        {
+            var todoItem = await _context.ToDoItem.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        // PUT: api/ToDoItems/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutToDoItem(long id, ToDoItem toDoItem)
+        {
+            if (id != toDoItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(toDoItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ToDoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ToDoItems
+        [HttpPost]
+        public async Task<ActionResult<ToDoItem>> PostToDoItem(ToDoItem toDoItem)
+        {
+            _context.ToDoItem.Add(toDoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetToDoItem", new { id = toDoItem.Id }, toDoItem);
+        }
+
+        // DELETE: api/ToDoItems/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ToDoItem>> DeleteToDoItem(long id)
+        {
+            var toDoItem = await _context.ToDoItem.FindAsync(id);
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.ToDoItem.Remove(toDoItem);
+            await _context.SaveChangesAsync();
+
+            return toDoItem;
+        }
+
+        private bool ToDoItemExists(long id)
+        {
+            return _context.ToDoItem.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class ToDoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool  isChecked {get;set;}
+    }
+}
